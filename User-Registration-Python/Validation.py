49984ea3c673cc0fbd83e9b82b1fb5f43f812886
“Samarth <samarthbm18@gmail.com>
2021-09-21 15:44:32 +0530

    [Samarth] Add: Method to validate last name.

--- User-Registration-Python/Validation.py
+++ User-Registration-Python/Validation.py
@@ -1,8 +1,8 @@
 '''
 * @Author: Samarth BM.
-* @Date: 2021-09-21 13:28  
+* @Date: 2021-09-21 15:41  
 * @Last Modified by: Samarth BM
-* @Last Modified time: 2021-09-21 13:28
+* @Last Modified time: 2021-09-21 15:41
 * @Title: To validate user details using regex.
 '''
 
@@ -25,6 +25,24 @@ class Validation():
         """        
         try:
             return bool(re.match("^[A-Z]{1}[a-z]{2,}$", firstName))
+
+        except Exception as e:
+            logger.error(e)
+
+    def validate_last_name(lastName):
+        """
+        Description:
+            This function is to validate last name.
+            Condition: Last name should start with Capital letter and should have min 2 letters.
+
+        Args:
+            lastName: Last name to validate
+
+        Returns:
+            boolean result
+        """        
+        try:
+            return bool(re.match("^[A-Z]{1}[a-z]{1,}$", lastName))
 
         except Exception as e:
             logger.error(e)
