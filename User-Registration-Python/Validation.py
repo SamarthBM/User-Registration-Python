2772f7358531dcbfc78cbbfea55c2527ecc386dd
“Samarth <samarthbm18@gmail.com>
2021-09-21 14:19:51 +0530

    [Samarth] Add: Method to validate first name.

--- User-Registration-Python/Validation.py
+++ User-Registration-Python/Validation.py
@@ -1,1 +1,30 @@
-print("Initial commit")
+'''
+* @Author: Samarth BM.
+* @Date: 2021-09-21 13:28  
+* @Last Modified by: Samarth BM
+* @Last Modified time: 2021-09-21 13:28
+* @Title: To validate user details using regex.
+'''
+
+import re
+from LogHandler import logger
+
+class Validation():
+
+    def validate_first_name(firstName):
+        """
+        Description:
+            This function is to validate first name.
+            Condition: Name should start with Capital letter and should have min 3 letters.
+
+        Args:
+            firstName: First name to validate
+
+        Returns:
+            boolean result
+        """        
+        try:
+            return bool(re.match("^[A-Z]{1}[a-z]{2,}$", firstName))
+
+        except Exception as e:
+            logger.error(e)
